49756be9d2baa4902382477b966c17c69fa04b72
lzl <247393862@qq.com>
2018-12-04 20:20:02 +0800

    把本地工程添加到git

--- hellogit/src/cn/itcat/test/HelloGit.java
+++ hellogit/src/cn/itcat/test/HelloGit.java
@@ -10,8 +10,9 @@ public class HelloGit {
         System.out.println("hellogit");
         System.out.println("hellogit");
         System.out.println("hellogit");
-
-
-
+    }
+    public  void test(){
+        int i=10;
+        i+=10;
     }
 }
